50b43d3a7fff407067bd6c6911c2e5d01065fa9d
Iryna <tichaya.13@gmail.com>
2023-09-04 20:42:14 +0300

    Изменения в файлах

--- seminar_testirovhick/seminar_2_dz_test.py
+++ seminar_testirovhick/seminar_2_dz_test.py
@@ -79,19 +79,19 @@
 
 
 
-n = int(input('Введите число монеток: '))
-from random import randint
-a, b = 0, 0
-for i in range(n):
-    temp = randint(0, 1)
-    print(temp, end=' ')
-    if temp > 0: a += 1
-    else: b += 1
-print()
-if a > b:
-    print(f'Нужно перевернуть {b} монеток')
-else:
-    print(f'Нужно перевернуть {a} монеток')
+# n = int(input('Введите число монеток: '))
+# from random import randint
+# a, b = 0, 0
+# for i in range(n):
+#     temp = randint(0, 1)
+#     print(temp, end=' ')
+#     if temp > 0: a += 1
+#     else: b += 1
+# print()
+# if a > b:
+#     print(f'Нужно перевернуть {b} монеток')
+# else:
+#     print(f'Нужно перевернуть {a} монеток')
 
 # Task_12
 # Петя и Катя – брат и сестра. Петя – студент, а Катя –
